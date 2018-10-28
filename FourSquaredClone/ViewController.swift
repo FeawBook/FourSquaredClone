17bc5e2ea61346f90fca351a0fe42e46a841073d
Thanathip Kumnarai <feaw.book@gmail.com>
2018-10-28 14:29:49 +0700

    test 2

--- FourSquaredClone/ViewController.swift
+++ FourSquaredClone/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         DispatchQueue.global(qos: .background).async {
             print("test")
+            print("test 2")
         }
         // Do any additional setup after loading the view, typically from a nib.
     }
